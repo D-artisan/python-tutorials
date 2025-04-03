ac2bc36da42503db791f4d0b25b6c8132ab011d2
d-artisan <dartisan.inc@gmail.com>
2025-04-03 10:56:48 +0100

    Updated OOP.py

--- OOP.py
+++ OOP.py
@@ -15,10 +15,10 @@ emp_1.last = 'Boakye'
 emp_1.email = 'dan@test.com'
 emp_1.pay = 1250000
 
-emp_2.first = 'Dan'
-emp_2.last = 'Boakye'
-emp_2.email = 'dan@test.com'
-emp_2.pay = 1250000
+emp_2.first = 'Qweku'
+emp_2.last = 'Sarfo'
+emp_2.email = 'qweku@test.com'
+emp_2.pay = 158400
 
-
-
+print(emp_1.email)
+print(emp_2.email)
