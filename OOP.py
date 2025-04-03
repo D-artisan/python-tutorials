bb99e101d68e0efaf380e463d8eb968dd62e7990
d-artisan <dartisan.inc@gmail.com>
2025-04-03 10:31:38 +0100

    Python OOP

---
+++ OOP.py
@@ -0,0 +1,24 @@
+# Python Object-Oriented Programming
+
+# Class is a blueprint for creating instances (objects)
+class Employee:
+    pass
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+emp_1.first = 'Dan'
+emp_1.last = 'Boakye'
+emp_1.email = 'dan@test.com'
+emp_1.pay = 1250000
+
+emp_2.first = 'Dan'
+emp_2.last = 'Boakye'
+emp_2.email = 'dan@test.com'
+emp_2.pay = 1250000
+
+
+
